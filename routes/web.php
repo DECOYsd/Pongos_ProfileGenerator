da47e6ae3fde764e86b8f7cd0b61e2bab2a89f5c
Jedo <portdecoy@gmail.com>
2026-04-13 21:28:32 +0800

    LAB U MAAM

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('index');
+});
+
+Route::post('/', function (Request $request) {
+    $validated = $request->validate([
+        'name' => 'required|string|max:255',
+        'age' => 'required|integer|min:1',
+        'program' => 'required|string|max:255',
+        'email' => 'required|email',
+        'gender' => 'required|in:male,female',
+        'hobbies' => 'required|array|min:5',
+        'hobbies.*' => 'required|string|max:255',
+        'biography' => 'required|string|max:1000',
+    ]);
+
+    $profiles = session('profiles', []);
+    $profiles[] = $validated;
+    session(['profiles' => $profiles]);
+
+    return redirect('/');
+});
+
+Route::post('/clear', function () {
+    session()->forget('profiles');
+    return redirect('/');
 });
